433d8ebb3e46b2a67d616303b23b408bdaef9ab8
watcharakit <gungii@Gun>
2014-09-03 22:28:23 +0700

    change position in DB.php

--- AccidentReportWebApplication/src/DB.php
+++ AccidentReportWebApplication/src/DB.php
@@ -1,6 +1,6 @@
 <?php
-    header('Content-Type: text/html; charset=utf-8');
-    require_once('AccidentReport.php');
+        header('Content-Type: text/html; charset=utf-8');
+        require_once('AccidentReport.php');
 	
 	function selectAllAccidentReport($con)
 	{
@@ -12,19 +12,19 @@
         
 	function selectAccidentReport ($con, $id)
 	{
-                $stmt = $con->prepare("SELECT Longitude,Latitude,AccidentType,
-									 AmountOfDead,AmountOfInjured,TrafficBlocked,
-				 					 Message,DateTime FROM AccidentReport WHERE ID = ?");
+                $stmt = $con->prepare("SELECT * FROM AccidentReport WHERE ID = ?");
                 $stmt->bind_param("i", $id);
                 $stmt->execute();
-                $stmt->bind_result($longitude,$latitude,$accidentType,
-								   $amountOfDead,$amountOfInjured,
-								   $trafficBlocked,$message,$dateTime);
-                $result = new AccidentReport($longitude,$latitude,$accidentType,
-                					$amountOfDead,$amountOfInjured,
-								    $trafficBlocked,$message,$dateTime);
+                $stmt->bind_result($id, $longitude,$latitude,$accidentType,
+                                        $amountOfDead,$amountOfInjured,
+                                        $trafficBlocked,$message,$dateTime);
+                
                 $stmt->fetch();
                 $stmt->close();
+                
+                $result = new AccidentReport($longitude,$latitude,$accidentType,
+                                                $amountOfDead,$amountOfInjured,
+						$trafficBlocked,$message,$dateTime);
                 return $result;
 	}
 	
